8676ae938a27b9c204dd2c03d4a6fed5adf100aa
Axel Pion <axel.pion@betc.com>
2022-05-11 10:53:05 +0200

    refactor(matrix): adjusting

--- exercises/Matrix.php
+++ exercises/Matrix.php
@@ -8,31 +8,39 @@ class Matrix {
         $this->matrix = $matrix;
     }
 
-    public function getColumn(): array
+    public function getColumn(int $choice): array
     {
+        if (strlen($this->matrix) === 1) {
+            return [$this->matrix];
+        }
         $column = [];
         $explode = explode(PHP_EOL, $this->matrix);
-        foreach ($explode as $matrix) {
+        foreach ($explode as $key => $matrix) {
             $exp = explode(" ", $matrix);
             foreach ($exp as $cKey => $item) {
                 $column[$cKey][] = $item ;
             }
         }
 
-        return $column;
+
+
+        return $column[$choice - 1];
     }
 
-    public function getRow(): array
+    public function getRow(int $choice): array
     {
+        if (strlen($this->matrix) === 1) {
+            return [$this->matrix];
+        }
         $row = [];
         $explode = explode(PHP_EOL, $this->matrix);
-        foreach ($explode as $matrix) {
-            $exp = explode (" ", $matrix);
-            foreach ($exp as $rKey => $item) {
-                $row[$rKey][] = $item;
+        foreach ($explode as $key => $matrix) {
+            $exp = explode(" ", $matrix);
+            foreach ($exp as $item) {
+                $row[$key][] = $item;
             }
         }
 
-        return $row;
+        return $row[$choice - 1];
     }
 }
--- index.php
+++ index.php
@@ -80,11 +80,12 @@ require 'exercises/Matrix.php';
 //
 //echo '<pre>' . var_export($school->studentsByGradeAlphabetical(), true) . '</pre>';
 
-$test = "9 8 7\n5 4 2\n6 6 7";
-$matrix = new Matrix($test);
-$column = $matrix->getColumn();
-echo '<pre>' . var_export($column, true) . '</pre>';
-
-$row = $matrix->getRow();
+$test = "1 2\n3 4";
+$test2 = "1 2 3\n4 5 6\n7 8 9";
+$matrix1 = new Matrix($test);
+$row = $matrix1->getRow(2);
 echo '<pre>' . var_export($row, true) . '</pre>';
+$matrix2 = new Matrix($test2);
+$column= $matrix2->getColumn(3);
+echo '<pre>' . var_export($column, true) . '</pre>';
 
